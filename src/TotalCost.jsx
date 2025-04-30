45dad9976d296dca3557d0d2476af1cc281ec6ee
Richard <rohde122195@gmail.com>
2025-04-30 11:53:36 -0400

    Final Commit

--- src/TotalCost.jsx
+++ src/TotalCost.jsx
@@ -3,20 +3,20 @@ import "./TotalCost.css";
 
 const TotalCost = ({ totalCosts, ItemsDisplay }) => {
   
-
-  return (
+    const total_amount = totalCosts.venue + totalCosts.av + totalCosts.meals;
+    return (
     <div className="pricing-app">
       <div className="display_box">
         <div className="header">
           <p className="preheading"><h3>Total cost for the event</h3></p>
         </div>
         <div>
-          <h2 id="pre_fee_cost_display" className="price">
-           
-          </h2>
+            <h2 id="pre_fee_cost_display" className="price">
+            ${total_amount}
+            </h2>
          
-            <div>
-             
+            <div className="render_items">
+            <ItemsDisplay />
             </div>
         </div>
       </div>
--- src/store.js
+++ src/store.js
@@ -1,8 +1,8 @@
 // store.js
 import { configureStore } from '@reduxjs/toolkit';
 import venueReducer from './venueSlice';
-import avReducer from 'avSlice.js';
-import mealsReducer from 'mealsSlice.js';
+import avReducer from './avSlice.js';
+import mealsReducer from './mealsSlice.js';
 
 export default configureStore({
   reducer: {
